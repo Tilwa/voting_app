c31c05300ea1c70ff8a2c2767e2f9aa9e47f52aa
shahrukh <50910055+Tilwa@users.noreply.github.com>
2021-09-19 16:12:09 +0500

    main component

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
+import React from 'react';
+class ProductList extends React.Component {
+    render() {
+      return (
+        <div className='ui unstackable items'>
+          Hello, friend! I am a basic React component.
+        </div>
+      );
+    }
+  }
 
+
+  export default ProductList;
