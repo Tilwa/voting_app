ec2db319508674414984ec9679d3549b0daec3a4
shahrukh <50910055+Tilwa@users.noreply.github.com>
2021-10-16 07:10:07 +0500

    props child content

--- public/js/Product.js
+++ public/js/Product.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Product extends React.Component {
-render() {
+  render() {
     return (
-    <div className='item'>
-        <div className='image'>
-        <img src='images/products/image-aqua.png' />
+      <div className="item">
+        <div className="image">
+          <img src="images/products/image-aqua.png" />
         </div>
-        <div className='middle aligned content'>
-        <div className='description'>
+        <div className="middle aligned content">
+          <div className="description">
             <a>Fort Knight</a>
             <p>Authentic renaissance actors, delivered in just two weeks.</p>
-        </div>
-        <div className='extra'>
+          </div>
+          <div className="extra">
             <span>Submitted by:</span>
-            <img
-            className='ui avatar image'
-            src='images/avatars/daniel.jpg'
-            />
+            <img className="ui avatar image" src="images/avatars/daniel.jpg" />
+          </div>
         </div>
-        </div>
-    </div>
+      </div>
     );
+  }
 }
-}
-    export default Product;
+export default Product;
--- public/js/app.js
+++ public/js/app.js
@@ -1,72 +1,25 @@
-
-class ProductList extends React.Component {
-  handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.');
-  }
-
-  render() {
-    const products = Seed.products.sort((a, b) => (
-      b.votes - a.votes
-    ));
-    const productComponents = products.map((product) => (
-      <Product
-        key={'product-' + product.id}
-        id={product.id}
-        title={product.title}
-        description={product.description}
-        url={product.url}
-        votes={product.votes}
-        submitterAvatarUrl={product.submitterAvatarUrl}
-        productImageUrl={product.submitterAvatarUrl}
-        onVote={this.handleProductUpVote}
-      />
-    ));
-    return (
-      <div className='ui unstackable items'>
-        {productComponents}
-      </div>
-    );
-  }
-}
-
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  // Inside `Product`
-  handleUpVote() {
-    this.props.onVote(this.props.id);
-  }
-
   render() {
     return (
-      <div className='item'>
-        <div className='image'>
+      <div className="item">
+        <div className="image">
           <img src={this.props.productImageUrl} />
         </div>
-        {/* Inside `render` for Product` */}
-        <div className='middle aligned content'>
-          <div className='header'>
-            <a onClick={this.handleUpVote}>
-              <i className='large caret up icon' />
+        <div className="middle aligned content">
+          <div className="header">
+            <a>
+              <i className="large caret up icon" />
             </a>
             {this.props.votes}
           </div>
-          <div className='description'>
-            <a href={this.props.url}>
-              {this.props.title}
-            </a>
-            <p>
-              {this.props.description}
-            </p>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
           </div>
-          <div className='extra'>
+          <div className="extra">
             <span>Submitted by:</span>
             <img
-              className='ui avatar image'
+              className="ui avatar image"
               src={this.props.submitterAvatarUrl}
             />
           </div>
@@ -75,8 +28,23 @@ class Product extends React.Component {
     );
   }
 }
+class ProductList extends React.Component {
+  render() {
+    const product = Seed.products[0];
+    return (
+      <div className="ui unstackable items">
+        <Product
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitterAvatarUrl={product.submitterAvatarUrl}
+          productImageUrl={product.productImageUrl}
+        />
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
-);
+ReactDOM.render(<ProductList />, document.getElementById("content"));
